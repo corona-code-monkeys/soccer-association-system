2c493b7f070ef3990c2ee39dbe3f1b4b76441720
MrBahur <MrBahur25@gmail.com>
2020-04-07 10:46:24 +0300

    SAS-22 #added GameEvent fields, constructor, getters and setters

--- src/main/java/com/SAS/game_event_logger/GameEvent.java
+++ src/main/java/com/SAS/game_event_logger/GameEvent.java
@@ -1,4 +1,39 @@
 package com.SAS.game_event_logger;
 
+import java.time.LocalDate;
+
 public abstract class GameEvent {
+    private String gameID;
+    private LocalDate gameDate;
+    private Integer gameMinute;
+
+    public GameEvent(String gameID, LocalDate gameDate, Integer gameMinute) {
+        this.gameID = gameID;
+        this.gameDate = gameDate;
+        this.gameMinute = gameMinute;
+    }
+
+    public String getGameID() {
+        return gameID;
+    }
+
+    public void setGameID(String gameID) {
+        this.gameID = gameID;
+    }
+
+    public LocalDate getGameDate() {
+        return gameDate;
+    }
+
+    public void setGameDate(LocalDate gameDate) {
+        this.gameDate = gameDate;
+    }
+
+    public Integer getGameMinute() {
+        return gameMinute;
+    }
+
+    public void setGameMinute(Integer gameMinute) {
+        this.gameMinute = gameMinute;
+    }
 }
