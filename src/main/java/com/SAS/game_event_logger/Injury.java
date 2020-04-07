1579a831738f04ff625d68fef7f4187287078c62
MrBahur <MrBahur25@gmail.com>
2020-04-07 11:43:55 +0300

    SAS-27 #added Injury fields, constructor, getters and setters

--- src/main/java/com/SAS/game_event_logger/Injury.java
+++ src/main/java/com/SAS/game_event_logger/Injury.java
@@ -1,4 +1,31 @@
 package com.SAS.game_event_logger;
 
+import java.time.LocalDate;
+
 public class Injury extends GameEvent {
+
+    private Player injuredPlayer;
+    private String description;
+
+    public Injury(String gameID, LocalDate gameDate, Integer gameMinute, Player injuredPlayer, String description) {
+        super(gameID, gameDate, gameMinute);
+        this.injuredPlayer = injuredPlayer;
+        this.description = description;
+    }
+
+    public Player getInjuredPlayer() {
+        return injuredPlayer;
+    }
+
+    public void setInjuredPlayer(Player injuredPlayer) {
+        this.injuredPlayer = injuredPlayer;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
