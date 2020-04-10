4019a05eb27659c67fa75c3f3dd912a9756c3989
Amit Nachimovitz <amitju@gmail.com>
2020-04-10 14:39:17 +0300

    SAS-20 #add report javadocs

--- src/main/java/com/SAS/soccer_association_system/report/GameReport.java
+++ src/main/java/com/SAS/soccer_association_system/report/GameReport.java
@@ -3,32 +3,59 @@ package com.SAS.soccer_association_system.report;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+/**
+ * The class represents the game report that made by the referee when a game ends
+ */
 public class GameReport {
 
     private LocalDate gameDate;
     private LocalDateTime time;
 
+    /**
+     * Empty constructor
+     */
     public GameReport() {
     }
 
+    /**
+     * Params constructor
+     * @param gameDate
+     * @param time
+     */
     public GameReport(LocalDate gameDate, LocalDateTime time) {
         this.gameDate = gameDate;
         this.time = time;
     }
 
 
+    /**
+     * The function returns the date the game took place
+     * @return
+     */
     public LocalDate getGameDate() {
         return gameDate;
     }
 
+    /**
+     * The function sets the date the game took place
+     * @return
+     */
     public void setGameDate(LocalDate gameDate) {
         this.gameDate = gameDate;
     }
 
+    /**
+     * The function returns the time the game ended
+     * @return
+     */
     public LocalDateTime getTime() {
         return time;
     }
 
+    /**
+     * The function sets the time the game ended
+     * @return
+     */
     public void setTime(LocalDateTime time) {
         this.time = time;
     }
--- src/main/java/com/SAS/soccer_association_system/report/PersonalReport.java
+++ src/main/java/com/SAS/soccer_association_system/report/PersonalReport.java
@@ -1,40 +1,76 @@
 package com.SAS.soccer_association_system.report;
 
+/**
+ * The class represent a complain report made by a registered user about the system
+ */
 public class PersonalReport {
 
     private int pageId;
     private String descr;
     private int userId;
 
+    /**
+     * Empty constructor
+     */
     public PersonalReport() {
     }
 
+    /**
+     * Params constructor
+     * @param pageId
+     * @param descr
+     * @param userId
+     */
     public PersonalReport(int pageId, String descr, int userId) {
         this.pageId = pageId;
         this.descr = descr;
         this.userId = userId;
     }
 
+    /**
+     * The function returns the pageId of the page that user complain about
+     * @return
+     */
     public int getPageId() {
         return pageId;
     }
 
+    /**
+     * The function sets the pageId of the page that user complain about
+     * @return
+     */
     public void setPageId(int pageId) {
         this.pageId = pageId;
     }
 
+    /**
+     * The function returns the description of the complain
+     * @return
+     */
     public String getDescr() {
         return descr;
     }
 
+    /**
+     * The function sets the description of the complain
+     * @return
+     */
     public void setDescr(String descr) {
         this.descr = descr;
     }
 
+    /**
+     * The function returns the userId of the user that complain
+     * @return
+     */
     public int getUserId() {
         return userId;
     }
 
+    /**
+     * The function sets the userId of the user that complain
+     * @return
+     */
     public void setUserId(int userId) {
         this.userId = userId;
     }
