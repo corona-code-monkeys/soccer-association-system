9e893fed96bf271fc083be74a3ad7f677392a2cc
dekellevy93 <dekelle@post.bgu.ac.il>
2020-04-20 11:22:54 +0300

    SAS-51: #added adder and setter to LeagueManagementCrud

--- src/main/java/com/SAS/crudoperations/LeagueManagementCRUD.java
+++ src/main/java/com/SAS/crudoperations/LeagueManagementCRUD.java
@@ -1,5 +1,8 @@
 package com.SAS.crudoperations;
 
+import com.SAS.League.League;
+import com.SAS.League.Season;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 public class LeagueManagementCRUD {
@@ -10,4 +13,26 @@ public class LeagueManagementCRUD {
         this.jdbcTemplate = jdbcTemplate;
     }
 
+
+    public int getLeague(String name) {
+        int id;
+        try {
+            String query = String.format("SELECT * FROM league WHERE league_name='"+name+"'");
+            id= jdbcTemplate.queryForObject(query, int.class);
+            return id;
+        }
+        catch (EmptyResultDataAccessException e) {
+            return -1;
+        }
+    }
+
+    public int setLeague(League league) {
+        int id=123456;
+        String query = String.format("insert into league values (%d, \"%s\");",league.getName(),id);//does i need to insert id or it will give it id automatticly
+        return jdbcTemplate.update(query);
+    }
+
+    public void addSeasonToLeague(int league, Season season) {
+
+    }
 }
