1a0db92ca8abfab9974a230ca2cf3c4e0d873b71
dekellevy93 <dekelle@post.bgu.ac.il>
2020-04-20 10:36:20 +0300

    SAS-52: #joining the usescases toghter in the controller

--- src/main/java/com/SAS/LeagueManagement/LeagueManagementController.java
+++ src/main/java/com/SAS/LeagueManagement/LeagueManagementController.java
@@ -1,6 +1,10 @@
 package com.SAS.LeagueManagement;
 
 import com.SAS.League.League;
+import com.SAS.League.Season;
+import com.SAS.team.Team;
+
+import java.util.HashSet;
 
 public class LeagueManagementController {
 
@@ -13,6 +17,17 @@ public class LeagueManagementController {
     public League initLeague(String name) {
         //check on DB if league exist controller.leagueExist
         League league= new League(name);
+        return league;
+    }
+
+    public League addSeasonToALeague(String name, int year) {
+        //check on DB if league exist controller.leagueExist
+        League league= crud.getLEague(name);
+        HashSet<Team> teams= new HashSet<>();
+        HashSet<League> leagues= new HashSet<>();
+        leagues.add(league);
+        Season season= new Season(year,teams,leagues);
+        league.addSeason(season);
         return league;
     }
 }
