1676843526ea3d7d2efdf87a553e5bd4436baf35
dekellevy93 <58995464+dekellevy93@users.noreply.github.com>
2020-04-22 20:02:45 +0300

    Update LeagueManagementControllerTest.java

--- src/test/java/com/SAS/LeagueManagement/LeagueManagementControllerTest.java
+++ src/test/java/com/SAS/LeagueManagement/LeagueManagementControllerTest.java
@@ -1,45 +1,45 @@
-//package com.SAS.LeagueManagement;
-//
-//import com.SAS.League.League;
-//import com.SAS.League.Season;
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.Test;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//class LeagueManagementControllerTest extends LeagueManagementController {
-//
-//    @Test
-//    void initLeague() {
-//        {
-//            String name = "Ligat Ha'al";
-//            LeagueManagementController LeagueInitUseCase = new LeagueManagementController();
-//            League league = LeagueInitUseCase.initLeague(name);
-//            try {
-//                Assertions.assertTrue(league.getName().equals(name));
-//            } catch (AssertionError e) {
-//                System.out.println("The name of the league (" + league.getName() + ") is not equal to the name inserted:(" + name + ")");
-//                throw e;
-//            }
-//            System.out.println("The name of the league (" + league.getName() + ") is equal to the name inserted:(" + name + ")");
-//
-//            try {
-//                Assertions.assertTrue(league instanceof League);
-//            } catch (AssertionError e) {
-//                System.out.println("The instance created isn't from the type of league");
-//                throw e;
-//            }
-//            System.out.println("The instance created is from the type of league");
-//            try {
-//                Assertions.assertFalse(league.getName().equals("asdasd"));
-//            } catch (AssertionError e) {
-//                System.out.println("The name of the league (" + league.getName() + ") is equal to the wrong name inserted:(asdasd)");
-//                throw e;
-//            }
-//            System.out.println("The name of the league (" + league.getName() + ") is not equal to the wrong name inserted:(asdasd)");
-//        }
-//    }
-//
+package com.SAS.LeagueManagement;
+
+import com.SAS.League.League;
+import com.SAS.League.Season;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LeagueManagementControllerTest extends LeagueManagementController {
+
+    @Test
+    void initLeague() {
+        {
+            String name = "Ligat Ha'al";
+            LeagueManagementController LeagueInitUseCase = new LeagueManagementController();
+            League league = LeagueInitUseCase.initLeague(name);
+            try {
+                Assertions.assertTrue(league.getName().equals(name));
+            } catch (AssertionError e) {
+                System.out.println("The name of the league (" + league.getName() + ") is not equal to the name inserted:(" + name + ")");
+                throw e;
+            }
+            System.out.println("The name of the league (" + league.getName() + ") is equal to the name inserted:(" + name + ")");
+
+            try {
+                Assertions.assertTrue(league instanceof League);
+            } catch (AssertionError e) {
+                System.out.println("The instance created isn't from the type of league");
+                throw e;
+            }
+            System.out.println("The instance created is from the type of league");
+            try {
+                Assertions.assertFalse(league.getName().equals("asdasd"));
+            } catch (AssertionError e) {
+                System.out.println("The name of the league (" + league.getName() + ") is equal to the wrong name inserted:(asdasd)");
+                throw e;
+            }
+            System.out.println("The name of the league (" + league.getName() + ") is not equal to the wrong name inserted:(asdasd)");
+       }
+    }
+
 //    @Test
 //    void addSeasonToALeague() {
 //        {
@@ -78,4 +78,4 @@
 //    void assignAndRemoveRefereesFromLeague() {
 //
 //    }
-//}
+}
