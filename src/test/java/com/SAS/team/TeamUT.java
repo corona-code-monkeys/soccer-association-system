6a8e8648c75d707e1a6cfef2121a061d588ec83e
Amit Nachimovitz <amitju@gmail.com>
2020-04-23 20:20:55 +0300

    SAS-71 #create teamUT class

---
+++ src/test/java/com/SAS/team/TeamUT.java
@@ -0,0 +1,83 @@
+package com.SAS.team;
+
+import com.SAS.User.Player;
+import com.SAS.User.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TeamUT {
+
+    private Team team;
+
+    @BeforeEach
+    void setUp() {
+        team = new Team();
+    }
+
+    @Test
+    void addValidPlayerToTeam() {
+    }
+
+    @Test
+    void addInvalidPlayerToTeam() {
+    }
+
+    @Test
+    void addSeason() {
+
+    }
+
+    @Test
+    void addTransactionToTeam() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void addFacility() {
+    }
+
+    @Test
+    void setTeamManager() {
+    }
+
+    @Test
+    void addTeamOwner() {
+    }
+
+    @Test
+    void setCoach() {
+    }
+
+    @Test
+    void removePlayer() {
+    }
+
+    @Test
+    void removeCoach() {
+    }
+
+    @Test
+    void removeFacility() {
+    }
+
+    @Test
+    void removeTeamManager() {
+    }
+
+    @Test
+    void inactivateTeam() {
+    }
+
+    @Test
+    void reactivateTeam() {
+    }
+
+    @Test
+    void isActive() {
+    }
+}
